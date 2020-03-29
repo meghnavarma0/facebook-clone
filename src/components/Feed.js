15b3ccf54ac6b07f8925dd81c162f327ea44b1d0
meghnavarma0 <meghnavarma0@gmail.com>
2020-03-29 23:24:05 +0530

    added favicon and updated site name

---
+++ src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<Container>
+			<Row>
+				<Col>Profile</Col>
+				<Col>Field</Col>
+				<Col>Empty</Col>
+			</Row>
+		</Container>
+	);
+};
---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<div className='outerBox'>
+				<TextInput label='First Name' />
+				<TextInput label='Last Name' />
+				<TextInput label='email' />
+				<TextInput label='Password' />
+			</div>
+		</div>
+	);
+};
