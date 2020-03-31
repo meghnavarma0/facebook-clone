22172085846bdad1a450e1868af2581ef51142c0
meghnavarma0 <meghnavarma0@gmail.com>
2020-04-01 00:18:29 +0530

    added fb icon to navbar

--- src/components/Reusable/Navbar.js
+++ src/components/Reusable/Navbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Navbar, NavItem, Icon, Container } from 'react-materialize';
 import { firebaseApp } from '../../firebase';
+import img from '../../assets/facebook.png';
 
 export default () => {
 	return (
-		<div style={{ background: 'royalblue' }}>
+		<div style={{ background: '#3b5998' }}>
 			<Container>
 				<Navbar
 					className='custom-navbar'
 					alignLinks='right'
 					brand={
 						<a className='brand-logo' href='www.google.com'>
-							Logo
+							<img
+								src={img}
+								alt='logo'
+								style={{
+									height: '40px',
+									backgroundColor: 'white',
+									marginTop: '12px'
+								}}
+							/>
 						</a>
 					}
 					id='mobile-nav'
